0c05ad8df82c67f58fb3eeb223db037f4a7f1129
Ryan Cook <rcook@redhat.com>
2016-08-25 23:37:07 -0400

    new files to prepare for ansible 2.x

---
+++ reference-architecture/aws-ansible/playbooks/lookup_plugins/ec2_zones_by_region.py
@@ -0,0 +1,16 @@
+from ansible import utils, errors
+import boto.ec2
+
+class LookupModule(object):
+    def __init__(self, basedir=None, **kwargs):
+        self.basedir = basedir
+
+    def run(self, region, inject=None, **kwargs):
+        try:
+            conn = boto.ec2.connect_to_region(region)
+            zones = [z.name for z in conn.get_all_zones()]
+            zones.remove('us-east-1b');
+            return zones
+        except e:
+            raise errors.AnsibleError("Could not lookup zones for region: %s\nexception: %s" % (region, e))
+
